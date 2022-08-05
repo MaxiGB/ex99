647310b1f6fed3c829517fe5024434712bd07037
Maxim Man <y1001777@yandex.ru>
2022-08-05 11:31:33 +0300

    риветствие с вводом имени!

---
+++ ex00599/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string? username = Console.ReadLine();
+
+if(username.ToLower() == "макс")
+{
+    Console.WriteLine("Ура, это же МАКС!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
