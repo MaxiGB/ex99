2e618d9f3817e0b70397f7aa646beb384b28c92a
Maxim Man <y1001777@yandex.ru>
2022-08-07 21:23:24 +0300

    почти дз

---
+++ dz001/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое цисло");
+int numberA=int.Parse (Console.ReadLine());
+Console.WriteLine("Введите второе цисло");
+int numberB=int.Parse (Console.ReadLine());
+if (numberA == numberB)
+{
+    Console.WriteLine("Первое число является квадратом втрого");
+}
+else 
+{
+        Console.WriteLine("Первое число НЕ является квадратом втрого");
+}
