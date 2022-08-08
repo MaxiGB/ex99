9e4854b707f8427cf40e7eb3566f8f8f8937ff16
Maxim Man <y1001777@yandex.ru>
2022-08-08 21:55:22 +0300

    еще один массив

---
+++ introArray2/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = {1, 12, 14,421, 331 ,31, 34};
+int n = array.Length;
+int find = 14;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+
+    }
+    //index = index +1;
+    index++;
+}
