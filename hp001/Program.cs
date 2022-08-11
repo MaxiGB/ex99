753111c166db681f5749520770ce2166e1344300
Maxim Man <y1001777@yandex.ru>
2022-08-11 10:59:49 +0300

    ДЗ!

---
+++ hp001/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine(Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).Max());
+
+Console.WriteLine((int.Parse(Console.ReadLine()) & 1) == 0? "Yes": "No");
+
+foreach (int i in Enumerable.Range(1, int.Parse(Console.ReadLine()) >> 1))
+
+Console.WriteLine(i << 1);
+
